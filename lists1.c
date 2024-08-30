aee635af7f9c9d581b86e7294f69bb6c6cc69d60
MOUSSA SABBAR <62625289+thatanoslord@users.noreply.github.com>
2024-08-30 04:04:09 +0100

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * shell_list_len - determines length of linked list
+ * list_len - determines length of linked list
  * @s: pointer to first node
  *
  * Return: size of list
  */
-size_t shell_list_len(const list_t *s)
+size_t list_len(const list_t *s)
 {
 	size_t i = 0;
 
@@ -19,12 +19,12 @@ size_t shell_list_len(const list_t *s)
 }
 
 /**
- * **shell_list_to_strings - returns an array of strings of the list->str
+ * list_to_strings - returns an array of strings of the list->str
  * @heads: pointer to first node
  *
  * Return: array of strings
  */
-char **shell_list_to_strings(list_t *heads)
+char **list_to_strings(list_t *heads)
 {
 	list_t *node = heads;
 	size_t i = list_len(heads), j;
@@ -56,12 +56,12 @@ char **shell_list_to_strings(list_t *heads)
 
 
 /**
- * shell_print_list - prints all elements of a list_t linked list
+ * print_list - prints all elements of a list_t linked list
  * @a: pointer to first node
  *
  * Return: size of list
  */
-size_t shell_print_list(const list_t *a)
+size_t print_list(const list_t *a)
 {
 	size_t i = 0;
 
@@ -79,14 +79,14 @@ size_t shell_print_list(const list_t *a)
 }
 
 /**
- * *shell_node_starts_with - returns node whose string starts with prefix
+ * node_starts_with - returns node whose string starts with prefix
  * @node: pointer to list head
  * @prefix: string to match
  * @d: the next character after prefix to match
  *
  * Return: match node or null
  */
-list_t *shell_node_starts_with(list_t *node, char *prefix, char d)
+list_t *node_starts_with(list_t *node, char *prefix, char d)
 {
 	char *p = NULL;
 
@@ -101,13 +101,13 @@ list_t *shell_node_starts_with(list_t *node, char *prefix, char d)
 }
 
 /**
- * shell_get_node_index - gets the index of a node
+ * get_node_index - gets the index of a node
  * @heads: pointer to list head
  * @node: pointer to the node
  *
  * Return: index of node or -1
  */
-ssize_t shell_get_node_index(list_t *heads, list_t *node)
+ssize_t get_node_index(list_t *heads, list_t *node)
 {
 	size_t i = 0;
 
